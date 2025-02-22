1e0936b217f9969468448c0195a6b390a95bad75
ImJustCookie <raphael@senellart.com>
2025-02-22 18:16:40 +0100

    simple v2 is done, random time for each word

---
+++ include/lorem.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+std::string lorem = "\nNo one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but "
+"\nbecause those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful."
+"\nNor again is there anyone who loves or pursues or desires to obtain pain of itself, "
+"\nbecause it is pain, but because occasionally circumstances "
+"\noccur in which toil and pain can procure him some great pleasure. "
+"\nTo take a trivial example, which of us ever undertakes"
+"\nlaborious physical exercise, except to obtain some advantage from it? "
+"\nBut who has any right to find fault with a man who "
+"\nchooses to enjoy a pleasure that has no annoying consequences,"
+"\n or one who avoids a pain that produces no resultant pleasure?";
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,74 @@
 #include <iostream>
 #include <string>
 #include <unistd.h>
+#include <random>
+#include <bits/stdc++.h>
+
+#include "lorem.hpp"
+
 using namespace std;
 
+
+int micro_sec_random(){
+    random_device rd;
+
+    mt19937 gen(rd());
+
+    uniform_int_distribution<> dis(50'000, 300'000);
+
+    return dis(gen);
+
+}
 
 void ecrire_betement(string texte){
     for (auto c : texte){
         cout << c;
         cout.flush();
-        usleep(100'000);
+        usleep(micro_sec_random());
     }
     cout << endl;
 }
 
 
+vector<string> splitBySpace(const string& input) {
+
+    istringstream iss(input);
+    vector<string> res;
+    string x;
+    string temp;
+    char delim = ' '; 
+    while (getline(iss, temp, delim)) {
+        res.push_back(temp);
+
+    }
+    return res;
+
+}
+
+void ecrire_moins_betement(string texte){
+    stringstream wordStream(texte);  
+    string word;
+    vector<string> listOfWords = splitBySpace(texte);
+
+    for (auto word : listOfWords){
+        int rand_speed = micro_sec_random();
+        for (auto c : word){
+            cout << c;
+            cout.flush();
+            usleep(rand_speed);
+        }
+        cout << ' ';
+        cout.flush();
+    }
+    cout << endl;
+
+
+}
+
+
+
 int main() {
-
-    string toto ="miam miam \ndes chocolats";
-
-    ecrire_betement(toto);
+    //ecrire_betement(toto);
+    ecrire_moins_betement(lorem);
     return 0;
 }
